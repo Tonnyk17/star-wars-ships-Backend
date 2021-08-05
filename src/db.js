6586e52c741ce6322c920d0d08d505f34b6f9e6c
Tonyk17 <legoton-naruto@hotmail.com>
2021-08-05 03:54:19 -0500

    deploy

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://Mr_Robot:Patata123@cluster0.flfdt.mongodb.net/test",{
+mongoose.connect("mongodb+srv://Mr_Robot:Patata123@cluster0.flfdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
