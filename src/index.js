2e89d6166ee200081fc6a88697c0f9404ede55bd
Tonyk17 <legoton-naruto@hotmail.com>
2021-08-05 03:25:45 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import { server } from './server';
+import {server} from './server';
 import './db';
 
 
-server.start({port: 8000}, ({port}) => {
-    console.log('server ready on port', port)
-})
+server.start();
